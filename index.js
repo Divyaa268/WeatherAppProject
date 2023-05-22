b0e529a179e313f0ad6cb481ec29731807d0b3fd
Divyaa268 <117614772+Divyaa268@users.noreply.github.com>
2023-05-22 22:13:30 +0530

    Added and updated JS file
    Added and updated JS file

---
+++ index.js
@@ -0,0 +1,7 @@
+const temperature = document.querySelector(".weather1");
+const location = document.querySelector(".weather2 p");
+const dateTime = document.querySelector(".weather2 span");
+const image = document.querySelector(".weather3 img");
+const condition = document.querySelector(".weather3 span");
+const seachCity = document.querySelector(".searchCity");
+const form = document.querySelector("form");
